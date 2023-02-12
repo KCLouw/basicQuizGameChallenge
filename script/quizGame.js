2b307b147bb5a540dcf3f20844e78f5bdb045b4a
KCLouw <klouw07@gmail.com>
2023-02-12 19:48:28 +0200

    started with the quiz

---
+++ script/quizGame.js
@@ -0,0 +1,13 @@
+alert(" You be assessed in a simple JavaScript Quiz game. \n When prompted questions please either choose 'T' for True or 'F' for False. \n Press OK to start");
+
+let correctAnswer = [];
+let preAnswers = ["F","T","T","F","T","F","F"];
+
+let q1 = prompt("JavaScript is primarily used for styling your webpage.");
+let q2 = prompt("JavaScript was inveted by Brendon Eich.");
+let q3 = prompt("JavaScript was invented in 1995.");
+let q4 = prompt("JavaScript was originally developed for the Mozilla Firefox browser.");
+let q5 = prompt("It is recommended to rather use the LET or CONST keywords for declaring variables and no longer the VAR keyword.");
+let q6 = prompt("JavaScript became the ECMA-262 standard in 2001.");
+let q7 = prompt("JavaScript and Java are the same programming language.");
+let q8 = prompt("JavaScript is primarily used as a scripting programming language.");
